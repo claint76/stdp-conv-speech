06c7b1df8c9229fbacc93c6b9c6f8bd2c3a09d3d
Meng Dong <whenov@gmail.com>
2017-07-04 17:34:21 +0800

    add plot.py

---
+++ output/plot.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+import pickle
+
+
+with open('params.json') as f:    
+    params = json.load(f)
+
+
+rows = 5
+cols = params['layers'][1]['map_num'] // rows
+
+with open('weights_layer_1.pickle', 'rb') as f:
+    w = pickle.load(f)
+w = w.reshape((params['layers'][1]['map_num'], params['layers'][0]['map_num'], params['layers'][1]['win_height'], params['layers'][1]['win_width']))
+w = w.transpose((0, 2, 3, 1))
+w = np.insert(w, 2, 0., axis=3)
+
+for i in range(params['layers'][1]['map_num']):
+    plt.subplot(rows, cols, i+1)
+    plt.axis('off')
+    plt.imshow(w[i], interpolation="nearest", vmin=0, vmax=1)
+
+plt.show()

cb327b31e46de5cc89b188d4fb82a879fe4ad714
Meng Dong <whenov@gmail.com>
2017-07-04 17:19:48 +0800

    move read_mnist() into mnist.py

--- main.py
+++ main.py
@@ -1,45 +1,16 @@
-import gzip
 import json
 import numpy as np
 import pickle
-from scipy.ndimage.filters import gaussian_filter
 
 import pycuda.driver as cuda
 import pycuda.autoinit
 from pycuda.compiler import SourceModule
 
+from mnist import read_mnist
 from network import Network
 
 
-print('Reading MNIST...')
-with gzip.open('mnist.pkl.gz', 'rb') as f:
-    train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
-
-print('Preprocessing with DoG filter...')
-new_train_set = (np.empty((50000, 784 * 2), dtype=np.float32), train_set[1])
-new_test_set = (np.empty((50000, 784 * 2), dtype=np.float32), test_set[1])
-for i in range(2):
-    data_set = (train_set, test_set)[i]
-    new_data_set = (new_train_set, new_test_set)[i]
-
-    for j in range(data_set[1].size):
-        im1 = gaussian_filter(data_set[0][j].reshape((28,28)), sigma=1)
-        im2 = gaussian_filter(data_set[0][j].reshape((28,28)), sigma=2)
-
-        im_on = im1 - im2
-        im_on[im_on < 0] = 0
-        im_on /= im_on.max()
-        im_on = im_on.reshape((784,))
-
-        im_off = im2 - im1
-        im_off[im_off < 0] = 0
-        im_off /= im_off.max()
-        im_off = im_off.reshape((784,))
-
-        new_data_set[0][j] = np.append(im_on, im_off)
-train_set = new_train_set
-test_set = new_test_set
-
+train_set, test_set = read_mnist()
 
 with open('params.json') as f:    
     params = json.load(f)
---
+++ mnist.py
@@ -0,0 +1,49 @@
+import gzip
+import numpy as np
+import pickle
+import os.path
+from scipy.ndimage.filters import gaussian_filter
+
+def read_mnist():
+    mnist_dog_file = 'mnist_dog.pickle'
+
+    if os.path.isfile(mnist_dog_file):
+        print('Reading {}...'.format(mnist_dog_file))
+        with open(mnist_dog_file, 'rb') as f:
+            train_set, test_set = pickle.load(f)
+    else:
+        print('Cannot find ' + mnist_dog_file)
+        print('Reading original MNIST...')
+        with gzip.open('mnist.pkl.gz', 'rb') as f:
+            train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
+        train_set = (np.append(train_set[0], valid_set[0], axis=0), np.append(train_set[1], valid_set[1], axis=0))
+
+        print('Preprocessing with DoG filter...')
+        new_train_set = (np.empty((60000, 784 * 2), dtype=np.float32), train_set[1])
+        new_test_set = (np.empty((10000, 784 * 2), dtype=np.float32), test_set[1])
+        for i in range(2):
+            data_set = (train_set, test_set)[i]
+            new_data_set = (new_train_set, new_test_set)[i]
+
+            for j in range(data_set[1].size):
+                im1 = gaussian_filter(data_set[0][j].reshape((28,28)), sigma=1)
+                im2 = gaussian_filter(data_set[0][j].reshape((28,28)), sigma=2)
+
+                im_on = im1 - im2
+                im_on[im_on < 0] = 0
+                im_on /= im_on.max()
+                im_on = im_on.reshape((784,))
+
+                im_off = im2 - im1
+                im_off[im_off < 0] = 0
+                im_off /= im_off.max()
+                im_off = im_off.reshape((784,))
+
+                new_data_set[0][j] = np.append(im_on, im_off)
+        train_set = new_train_set
+        test_set = new_test_set
+
+        with open(mnist_dog_file, 'wb') as f:
+            pickle.dump((train_set, test_set), f)
+
+    return train_set, test_set

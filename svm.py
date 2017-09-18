7d9db93a8c2b51bcbeac6ff8d3fda81e9893824e
Meng Dong <whenov@gmail.com>
2017-09-18 12:16:39 +0800

    update svm.py

--- svm.py
+++ svm.py
@@ -3,9 +3,12 @@
 from sklearn import svm
 from readers.tidigits import read_data
 
-train_set, test_set = read_data()
+n_bands = 41
+n_frames = 40
+
+train_set, test_set = read_data(n_bands, n_frames)
 
 clf = svm.SVC(kernel='linear')
-clf.fit(train_set[0].reshape((932, 40*26)), train_set[1])
-accuracy = clf.score(test_set[0].reshape((932, 40*26)), test_set[1])
+clf.fit(train_set[0].reshape((-1, n_bands * n_frames)), train_set[1])
+accuracy = clf.score(test_set[0].reshape((-1, n_bands * n_frames)), test_set[1])
 print('Accuracy:', accuracy * 100)

94207b178f59c6131e8569481860a3e3e7d2ac5b
Meng Dong <whenov@gmail.com>
2017-11-04 23:24:47 +0800

    update weights figure

--- figures/weights.py
+++ figures/weights.py
@@ -10,18 +10,18 @@ with open('../params_globalpool.json') as f:
     params = json.load(f)
 
 
-rows = 10
+rows = 4
 cols = params['layers'][1]['map_num'] // rows
 
 with open('../output/weights_layer_1.pickle', 'rb') as f:
     w = pickle.load(f)
 w = w.reshape((params['layers'][1]['sec_num'], params['layers'][1]['map_num'], params['layers'][1]['win'][0], params['layers'][1]['win'][1]))
 
-plt.figure(figsize=(5.2, 3.5))
+plt.figure(figsize=(6.5, 8.75))
 for j in range(params['layers'][1]['map_num']):
     plt.subplot(rows, cols, j+1)
     plt.axis('off')
-    plt.imshow(w[5][j], interpolation="nearest", vmin=0, vmax=1)
+    plt.imshow(w[5][j].transpose(), interpolation="nearest", vmin=0, vmax=1)
 
 plt.savefig('weights.png')
 plt.show()

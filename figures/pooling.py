689ff151edbf379fccb20d2f0d9ca70e5e77cdb7
Meng Dong <whenov@gmail.com>
2017-11-20 11:11:36 +0800

    reduce pooling figure size

--- figures/pooling.py
+++ figures/pooling.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 with open('../output/output_test_set.pickle', 'rb') as f:
     test_set = pickle.load(f)
 
-fig, axes = plt.subplots(5, 2, figsize=(7, 6))
+fig, axes = plt.subplots(5, 2, figsize=(4.5, 4))
 
 for i in range(10):
     idxes = np.where(test_set[1] == i)[0]

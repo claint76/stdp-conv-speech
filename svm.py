0961626e8bfad486bb41aa583e5b1a9bc7ff135b
Meng Dong <whenov@gmail.com>
2017-11-27 17:59:37 +0800

    add svm.py

---
+++ svm.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from sklearn import svm
+from readers.tidigits import read_data
+
+n_bands=40
+n_frames=41
+
+train_set, test_set = read_data(path='data/tidigits', n_bands=n_bands, n_frames=n_frames)
+
+clf = svm.SVC(kernel='linear')
+clf.fit(train_set[0].reshape((-1, n_bands * n_frames)), train_set[1])
+accuracy = clf.score(test_set[0].reshape((-1, n_bands * n_frames)), test_set[1])
+print('Accuracy:', accuracy * 100)

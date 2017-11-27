e2b27cef4d1fb57c58063a719c8e104310315f5e
Meng Dong <whenov@gmail.com>
2017-11-27 17:02:10 +0800

    modify t-sne figure

--- figures/t-sne.py
+++ figures/t-sne.py
@@ -26,12 +26,17 @@ with open('../output/output_test_set.pickle', 'rb') as f:
     output_test_set = pickle.load(f)
 
 
-fig, axes = plt.subplots(1, 2)
+fig, axes = plt.subplots(1, 2, figsize=(5.2, 4))
 
 X_tsne = TSNE().fit_transform(test_set[0])
 axes[0].scatter(X_tsne[:, 0], X_tsne[:, 1], c=test_set[1], cmap='tab10')
+axes[0].axis('off')
+axes[0].set_title('(A) t-SNE of original data')
 
 X_tsne = TSNE().fit_transform(output_test_set[0])
 axes[1].scatter(X_tsne[:, 0], X_tsne[:, 1], c=output_test_set[1], cmap='tab10')
+axes[1].axis('off')
+axes[1].set_title('(B) t-SNE of SNN output')
 
+fig.savefig('t-sne.png')
 plt.show()

fb32ab2b28cf61f2d94c080296a645b6ff4f1435
Meng Dong <whenov@gmail.com>
2017-12-07 10:30:10 +0800

    update both svm.py

--- output/svm.py
+++ output/svm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-import pickle
 from sklearn import svm
+import pickle
 
 
 with open('output_train_set.pickle', 'rb') as f:
@@ -9,7 +9,7 @@ with open('output_train_set.pickle', 'rb') as f:
 with open('output_test_set.pickle', 'rb') as f:
     test_set = pickle.load(f)
 
-clf = svm.SVC(kernel='linear', verbose=True)
+clf = svm.SVC(kernel='linear')
 clf.fit(train_set[0], train_set[1])
-score = clf.score(test_set[0], test_set[1])
-print(score)
+accuracy = clf.score(test_set[0], test_set[1])
+print('Accuracy:', accuracy * 100)
--- svm.py
+++ svm.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 from sklearn import svm
 from readers.tidigits import read_data

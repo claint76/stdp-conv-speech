58c77a845483228205932eee99361b484a4925cb
Meng Dong <whenov@gmail.com>
2017-12-07 16:03:45 +0800

    update accuracy figure

--- figures/accuracy.py
+++ figures/accuracy.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 
 x = np.loadtxt('../output/test_output')
 
-plt.figure(figsize=(5.2, 3.2))
-plt.plot(x[0:1000:5,0], x[0:1000:5,1])
+plt.figure(figsize=(5.2, 2.8))
+plt.plot(x[0:1000:5,0], x[0:1000:5,1], label='SVM accuracy on SNN output')
+plt.axhline(y=95, color='orange', linestyle='--', label='SVM accuracy on MFSC features')
 plt.xlabel('Number of training samples')
 plt.ylabel('Accuracy')
+plt.legend()
 plt.tight_layout()
 
 plt.savefig('accuracy.png')

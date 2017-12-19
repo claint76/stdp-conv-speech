a0fcf49f543cac27b38688254748b3548f0978ee
Meng Dong <whenov@gmail.com>
2017-12-19 16:26:57 +0800

    update pooling figure

--- figures/pooling.py
+++ figures/pooling.py
@@ -18,6 +18,12 @@ for i in range(10):
     ax.axis('off')
     ax.set_title('Digit ' + str(i))
 
+axes[4, 0].arrow(-0.5, 12, 50, 0, head_width=2, head_length=3, fc='k', ec='k', length_includes_head=True, overhang=0.3, clip_on=False)
+axes[4, 0].arrow(-4, 8.5, 0, -9, head_width=2, head_length=3, fc='k', ec='k', length_includes_head=True, overhang=0.3, clip_on=False)
+axes[4, 0].text(25, 13.5, 'Feature maps', verticalalignment='top', horizontalalignment='center', size='medium')
+axes[4, 0].text(-5.5, 4, 'Sections', verticalalignment='center', horizontalalignment='right', size='medium', rotation=90)
+
 fig.tight_layout()
+fig.subplots_adjust(bottom=0.08, left=0.08)
 fig.savefig('pooling.png')
 plt.show()

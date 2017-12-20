ae9fb869e0095684a213039acf6e72e773f4d61e
Meng Dong <whenov@gmail.com>
2017-12-20 15:32:07 +0800

    update weights_changing figure

--- figures/weights_changing.py
+++ figures/weights_changing.py
@@ -12,7 +12,7 @@ with open('../params.json') as f:
 
 m = 3  # features
 n = 12 # time points
-fig, axes = plt.subplots(n, m, figsize=(5.2, 4.5))
+fig, axes = plt.subplots(n, m, figsize=(4.5, 4.0))
 
 for i in range(n):
     with open('../output/weights_layer_1_{}.pickle'.format(i * 2000), 'rb') as f:
@@ -25,15 +25,15 @@ for i in range(n):
     axes[i, 2].axis('off')
     axes[i, 2].imshow(w[5][34], interpolation="nearest", vmin=0, vmax=1)
 
-fig.subplots_adjust(left=0.06, right=0.94, bottom=0.05, top=0.95, wspace=0.4, hspace=0.5)
+fig.subplots_adjust(left=0.06, right=0.94, bottom=0.05, top=0.95, wspace=0.4, hspace=0.01)
 fig.savefig('weights_changing.png')
 img = image.imread('weights_changing.png')
 
-fig, axes = plt.subplots()
+fig, axes = plt.subplots(figsize=(5.2, 4.5))
 axes.imshow(img)
-axes.set_yticks([y for y in range(101, 1400, 104)])
+axes.set_yticks([y for y in range(101, 1100, 90)])
 axes.set_yticklabels([y*2000 for y in range(12)])
-axes.set_xticks([280, 790, 1300])
+axes.set_xticks([250, 690, 1130])
 axes.set_xticklabels(['Feature map #1', 'Feature map #2', 'Feature map #3'])
 axes.set_ylabel('Number of training samples')
 

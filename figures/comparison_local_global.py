3cba0b30ee217d8977c66ba13fa0e8bcf1bec6c9
Meng Dong <whenov@gmail.com>
2017-12-22 13:01:01 +0800

    update comparison_local_global figure

--- figures/comparison_local_global.py
+++ figures/comparison_local_global.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 
 xl = np.loadtxt('accuracy_local')
 xg = np.loadtxt('accuracy_global')
+max_maps = 9
 
 plt.figure(figsize=(5.2, 3.2))
-plt.errorbar(xl[:, 0], xl[:, 1:].mean(axis=1), yerr=np.std(xl[:, 1:], axis=1, ddof=1), fmt='-o', capsize=3, label='Local weight sharing')
-plt.errorbar(xg[:, 0], xg[:, 1:].mean(axis=1), yerr=np.std(xg[:, 1:], axis=1, ddof=1), fmt='-s', capsize=3, label='Global weight sharing')
+plt.errorbar(xl[:max_maps, 0], xl[:max_maps, 1:].mean(axis=1), yerr=np.std(xl[:max_maps, 1:], axis=1, ddof=1), fmt='-o', capsize=3, label='Local weight sharing')
+plt.errorbar(xg[:max_maps, 0], xg[:max_maps, 1:].mean(axis=1), yerr=np.std(xg[:max_maps, 1:], axis=1, ddof=1), fmt='-s', capsize=3, label='Global weight sharing')
 plt.ylim([90, 100])
 plt.xlabel('Number of feature maps')
 plt.ylabel('Accuracy')

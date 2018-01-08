5b1be5f8e15e3472bdf783e50f0be13f349261b0
Meng Dong <whenov@gmail.com>
2018-01-08 17:09:41 +0800

    update accuracy curve figure and data

--- figures/accuracy.py
+++ figures/accuracy.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 x = np.loadtxt('test_accuracies')
 
 plt.figure(figsize=(5.2, 3.1))
-plt.plot(x[0:1000:5,0], x[0:1000:5,1], label='SVM accuracy on SNN output')
+plt.errorbar(x[:, 0], x[:, 1:].mean(axis=1), yerr=np.std(x[:, 1:], axis=1, ddof=1), fmt='-o', capsize=3, label='SVM accuracy on SNN output')
 plt.axhline(y=95, color='orange', linestyle='--', label='SVM accuracy on MFSC features')
 plt.xlabel('Number of training samples')
 plt.ylabel('Accuracy')

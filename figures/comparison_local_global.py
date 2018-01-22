c3a17f396dc2c6ed2d7d50e2a56bcd96d2f6815c
Meng Dong <whenov@gmail.com>
2018-01-22 23:19:00 +0800

    update comparison_local_global figure

--- figures/comparison_local_global.py
+++ figures/comparison_local_global.py
@@ -8,8 +8,8 @@ min_maps = 0
 max_maps = 9
 
 plt.figure(figsize=(5.2, 3.2))
-plt.errorbar(xl[min_maps:max_maps, 0], xl[min_maps:max_maps, 1:].mean(axis=1), yerr=np.std(xl[min_maps:max_maps, 1:], axis=1, ddof=1), fmt='-o', capsize=3, label='Local weight sharing')
-plt.errorbar(xg[min_maps:max_maps, 0], xg[min_maps:max_maps, 1:].mean(axis=1), yerr=np.std(xg[min_maps:max_maps, 1:], axis=1, ddof=1), fmt='-s', capsize=3, label='Global weight sharing')
+plt.errorbar(xl[min_maps:max_maps, 0], xl[min_maps:max_maps, 1:].mean(axis=1), yerr=np.std(xl[min_maps:max_maps, 1:], axis=1, ddof=1), fmt='-v', markersize=4, capsize=3, label='Local weight sharing')
+plt.errorbar(xg[min_maps:max_maps, 0], xg[min_maps:max_maps, 1:].mean(axis=1), yerr=np.std(xg[min_maps:max_maps, 1:], axis=1, ddof=1), fmt='-^', markersize=4, capsize=3, label='Global weight sharing')
 plt.ylim([85, 100])
 plt.xlabel('Number of feature maps')
 plt.ylabel('Accuracy')
